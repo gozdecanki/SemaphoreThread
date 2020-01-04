dd5ff62272e8505b6e380ff4676f10b8423973d3
Gözde Cankı <gozdecanki@gmail.com>
2020-01-04 13:07:53 +0300

    Semaphore Threading Implementation

---
+++ SemaphoreThread/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+
+namespace SemaphoreThread
+{
+    class Program
+    {
+
+        public static SemaphoreSlim SemaphoreSlim = new SemaphoreSlim(6);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Otoparka Hoşgeldiniz!");
+
+            for (int i = 1; i <= 10; i++)
+            {
+                new Thread(EnterThePark).Start(i);
+
+
+            }
+
+            Console.ReadLine();
+        }
+
+        public static void EnterThePark(object id)
+        {
+            Console.WriteLine(id + " kuyrukta");
+            SemaphoreSlim.Wait();
+            Console.WriteLine(id + " park etti");
+            Thread.Sleep(1000 * (int)id);
+            Console.WriteLine(id + " ayrılıyor");
+            SemaphoreSlim.Release();
+
+        }
+        
+
+
+       
+    }
+}
